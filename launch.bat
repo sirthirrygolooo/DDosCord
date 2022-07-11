8d25378d3488a9086b1a69f0c0523c558eb45f43
sirthirrygolooo <72656353+sirthirrygolooo@users.noreply.github.com>
2022-07-11 15:37:22 +0200

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -4,16 +4,13 @@ REM nomme la fenêtre
 title DDoSCord Lancher 
 cls
 
-REM definition de la fonction principale  
-
 :MAIN
 cls
 echo. +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-echo. + Veuillez presser S pour lancer, V pour verifier votre version de python (3.9 idealement) et             +
-echo. + eviter des problemes de compatibilite, I pour installer les bibliotheques requises et E pour quitter... +
+echo. + Press S to start, V for checking python version (3.9 is good) and avoid some compatibilities            +
+echo. + problems, I to install requirements and E to exit...                                                    +
 echo. +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-REM choix de l'utilisateur avec redirection vers la fonction qui correspond a l'action voulue
 
 choice /c SVIE /m ">>>"
 IF ERRORLEVEL 4 GOTO END
@@ -21,25 +18,23 @@ IF ERRORLEVEL 3 GOTO INS
 IF ERRORLEVEL 2 GOTO VER
 IF ERRORLEVEL 1 GOTO START 
 
-REM fonction qui lance juste le programme
+
 :START
 python main.py
 exit
 
-REM montre à l'utilisateur sa version de python
+
 :VER
 cls
 python --version
 pause
 GOTO MAIN 
 
-REM Installe les bibliothèques requises contenues dans le fichier requirements.txt avec pip
 :INS 
 pip install -r requirements.txt
 pause
 GOTO MAIN
 
-REM Fonction qui permet de quitter le programme
 :END
 CHOICE /c YN /m "Voulez vous vraiment quitter ? "
 
